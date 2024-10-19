7687bb2b2219be5776201abf0f2daa867ab25b9b
Melody Cherry <melody.laffitau@gmail.com>
2024-10-19 13:21:23 +0200

    Add files via upload

---
+++ ft_isalpha.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 16:25:10 by mlaffita          #+#    #+#             */
+/*   Updated: 2024/10/10 16:51:54 by mlaffita         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char	test1 = 'a';
+	char	test2 = 'B';
+	char	test3 = '0';
+
+	printf("Test avec %c, : %d\n",test1, ft_isalpha(test1));
+	printf("Test avec %c, : %d\n",test2, ft_isalpha(test2));
+	printf("Test avec %c, : %d\n",test3, ft_isalpha(test3));
+	return (0);
+}
+*/
