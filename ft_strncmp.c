6f7c85e1bfbfa05f1ece9a3afb356a0f96124787
Melody Cherry <melody.laffitau@gmail.com>
2024-10-25 17:49:20 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mlaffita <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:25:23 by mlaffita          #+#    #+#             */
-/*   Updated: 2024/10/23 14:44:34 by mlaffita         ###   ########.fr       */
+/*   Updated: 2024/10/25 17:44:51 by mlaffita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	while ((unsigned char)s1[i] != '\0'
+		&& (unsigned char)s2[i] != '\0' && i < n - 1)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 /*
 #include <stdio.h>
